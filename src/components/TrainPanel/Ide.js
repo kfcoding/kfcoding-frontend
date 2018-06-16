e3268d60ad50a9c94f04e36a6a7505c2e8508e65
guodong <gd@tongji.edn.cn>
2018-06-16 21:45:24 +0800

    use ide component

---
+++ src/components/TrainPanel/Ide.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { Select } from 'antd';
+import AceEditor from 'react-ace';
+
+const Option = Select.Option;
+
+const languages = [
+  'javascript',
+  'java',
+  'python',
+  'xml',
+  'ruby',
+  'sass',
+  'markdown',
+  'mysql',
+  'json',
+  'html',
+  'handlebars',
+  'golang',
+  'csharp',
+  'elixir',
+  'typescript',
+  'css'
+]
+
+const themes = [
+  'monokai',
+  'github',
+  'tomorrow',
+  'kuroir',
+  'twilight',
+  'xcode',
+  'textmate',
+  'solarized_dark',
+  'solarized_light',
+  'terminal',
+]
+
+languages.forEach((lang) => {
+  require(`brace/mode/${lang}`)
+  require(`brace/snippets/${lang}`)
+})
+
+themes.forEach((theme) => {
+  require(`brace/theme/${theme}`)
+})
+
+class Ide extends React.Component {
+  state = {
+    mode: 'python',
+    theme: 'monokai'
+  }
+
+  changeMode = (value) => {
+    this.setState({mode: value})
+  }
+
+  changeTheme = (value) => {
+    this.setState({theme: value})
+  }
+
+  render() {
+    return (
+      <div style={{height: '100%'}}>
+        <div style={{padding: '5px', background: '#fff'}}>
+          <span style={{marginRight: '10px'}}>语言：<Select defaultValue="python" style={{ width: 120 }} onChange={this.changeMode}>
+          {languages.map(lang => {
+            return <Option key={lang} value={lang}>{lang}</Option>
+          })}
+        </Select>
+            </span>
+          样式：<Select defaultValue="monokai" style={{ width: 120 }} onChange={this.changeTheme}>
+          {themes.map(lang => {
+            return <Option key={lang} value={lang}>{lang}</Option>
+          })}
+        </Select>
+        </div>
+        <AceEditor
+          mode={this.state.mode}
+          theme={this.state.theme}
+          fontSize={15}
+          name={new Date().getTime() + ''}
+          editorProps={{$blockScrolling: true}}
+          style={{width: '100%', height: '100%'}}
+          setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+            enableSnippets: true,
+            showLineNumbers: true,
+            tabSize: 2,
+          }}
+          value={`def print_welcome(name):
+    print("Welcome ", name)
+
+  print_welcome("Kfcoding")
+  `}
+        />
+      </div>
+    );
+  }
+}
+
+export default Ide;
--- src/components/TrainPanel/index.js
+++ src/components/TrainPanel/index.js
@@ -4,9 +4,7 @@ import Term from '../Term';
 import { createTerminal, createCloudware } from "../../services/kongfu";
 import './style.css';
 import Cloudware from "./Cloudware";
-import AceEditor from 'react-ace';
-import 'brace/mode/python';
-import 'brace/theme/xcode';
+import Ide from './Ide';
 
 const TabPane = Tabs.TabPane;
 
@@ -32,26 +30,7 @@ class TrainPanel extends React.Component {
     if (type == 'ide') {
       panes.push({
         title: 'WebIDE-' + idx,
-        content: <AceEditor
-          mode="python"
-          theme="xcode"
-          fontSize={15}
-          name={idx + ''}
-          editorProps={{$blockScrolling: true}}
-          style={{width: '100%', height: '100%'}}
-          setOptions={{
-            enableBasicAutocompletion: true,
-            enableLiveAutocompletion: true,
-            enableSnippets: true,
-            showLineNumbers: true,
-            tabSize: 2,
-          }}
-          value={`def print_welcome(name):
-  print("Welcome ", name)
-
-print_welcome("Kfcoding")
-`}
-        />,
+        content: <Ide/>,
         key: idx + ''
       })
       this.setState({panes, activeKey})
