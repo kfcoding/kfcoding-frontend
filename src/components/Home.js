250c3c43fb06a81e660efd7ea2e861f22a3b98d3
mapleB <515186469@qq.com>
2018-06-22 12:13:14 +0800

    格式

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import styles from './Library.css';
-import {message, Modal, Layout, Divider, Button, Menu, Icon } from 'antd';
-import { Link } from 'react-router-dom';
+import {message, Modal, Layout, Divider, Button, Menu, Icon} from 'antd';
+import {Link} from 'react-router-dom';
 import Book from "../components/Book";
-import { getMyKongfu } from "../services/users";
-import { createKongfu, deleteKongfu } from "../services/kongfu";
+import {getMyKongfu} from "../services/users";
+import {createKongfu, deleteKongfu} from "../services/kongfu";
 import MyHeader from "./Header";
 import MyFooter from "./Footer";
 
-const { Content, Sider } = Layout;
+const {Content, Sider} = Layout;
 const ButtonGroup = Button.Group;
-const { SubMenu } = Menu;
+const {SubMenu} = Menu;
 const confirm = Modal.confirm;
 
 class Home extends React.Component {
@@ -25,29 +25,29 @@ class Home extends React.Component {
 
 
   showDeleteConfirm(id) {
-      console.log(this.state);
-      const self = this;
-      confirm({
-          title: '确定删除该功夫？',
-          content: '',
-          okText: '确定',
-          okType: 'danger',
-          cancelText: '取消',
-          onOk() {
-            deleteKongfu(id).then(res => {
-              if (res.data.code == 200){
-                message.success("删除成功");
-                // 重新加载
-                getMyKongfu().then(res => {
-                    self.setState({kongfus: res.data.result.kongfuList})
-                })
-              }
+    console.log(this.state);
+    const self = this;
+    confirm({
+      title: '确定删除该功夫？',
+      content: '',
+      okText: '确定',
+      okType: 'danger',
+      cancelText: '取消',
+      onOk() {
+        deleteKongfu(id).then(res => {
+          if (res.data.code == 200) {
+            message.success("删除ç成功");
+            // 重新加载
+            getMyKongfu().then(res => {
+              self.setState({kongfus: res.data.result.kongfuList})
             })
-          },
-          onCancel() {
-              console.log('Cancel');
-          },
-      });
+          }
+        })
+      },
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
   }
 
   componentWillMount() {
@@ -61,7 +61,6 @@ class Home extends React.Component {
       visible: true,
     });
   }
-
 
 
   handleCancel = () => {
@@ -108,8 +107,8 @@ class Home extends React.Component {
               <Button type="primary" icon="eye" href={viewhref}>阅读</Button>
               <Button type="primary" icon="edit" href={edithref}>编辑</Button>
               <Button type="primary" icon="setting" href={settinghref}>设置</Button>
-              <Button onClick={()=>this.showDeleteConfirm(kf.id)} type="primary" icon="delete">
-                  删除
+              <Button onClick={() => this.showDeleteConfirm(kf.id)} type="primary" icon="delete">
+                删除
               </Button>
             </ButtonGroup>
           </div>
@@ -121,16 +120,16 @@ class Home extends React.Component {
     return (
       <Layout>
         <MyHeader/>
-        <Content style={{ padding: '50px' }}>
-          <Layout style={{ padding: '24px 0', background: '#fff' }}>
-            <Sider width={200} style={{ background: '#fff' }}>
+        <Content style={{padding: '50px'}}>
+          <Layout style={{padding: '24px 0', background: '#fff'}}>
+            <Sider width={200} style={{background: '#fff'}}>
               <Menu
                 mode="inline"
                 defaultSelectedKeys={['1']}
                 defaultOpenKeys={['sub1']}
-                style={{ height: '100%' }}
+                style={{height: '100%'}}
               >
-                <SubMenu key="sub1" title={<span><Icon type="wallet" />藏经阁</span>}>
+                <SubMenu key="sub1" title={<span><Icon type="wallet"/>藏经阁</span>}>
                   <Menu.Item key="1">我创建的秘籍</Menu.Item>
                   {/*<Menu.Item key="2">我收藏的秘籍</Menu.Item>*/}
                 </SubMenu>
@@ -138,11 +137,11 @@ class Home extends React.Component {
               </Menu>
             </Sider>
             <Content>
-              <div style={{ background: '#fff', padding: 24, minHeight: 280, overflow: 'auto' }}>
+              <div style={{background: '#fff', padding: 24, minHeight: 280, overflow: 'auto'}}>
                 <Divider orientation="left" style={{fontSize: '28px'}}>藏经阁</Divider>
                 {kongfus}
                 <Link to='/kongfu/create'>
-                  <div className='container' >
+                  <div className='container'>
                     <div className='book'>
                       <div className='front'>
                         <div className='addCover' style={{backgroundColor: '#525485'}}>
