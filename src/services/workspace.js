450c12cd077fda233b258e4ea477a59f694a8392
mapleB <515186469@qq.com>
2018-07-04 13:11:15 +0800

    修改url

--- src/services/workspace.js
+++ src/services/workspace.js
@@ -2,7 +2,7 @@ import request from '../utils/request';
 import API from "../utils/api";
 
 export function createWorkSpace(data) {
-  return request(API + '/workspace/create', {
+  return request(API + '/workspaces', {
     headers: {
       'Content-Type': 'application/json'
     },
